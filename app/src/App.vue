ea811897c0a48911913c6187588b83da7bfe5e26
rayque <rayque.oliveira@gmail.com>
2021-03-16 22:12:47 -0400

    Add form

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,18 +2,13 @@
   <v-app id="app">
     <v-app-bar
         app
-        color="white"
+        color="orange"
         flat
     >
-      <v-avatar
-          :color="$vuetify.breakpoint.smAndDown ? 'grey darken-1' : 'transparent'"
-          size="32"
-      ></v-avatar>
-
       <v-tabs
           centered
           class="ml-n9"
-          color="grey darken-1"
+          color="white"
       >
         <v-tab
             v-for="link in links"
@@ -23,15 +18,11 @@
         </v-tab>
       </v-tabs>
 
-      <v-avatar
-          class="hidden-sm-and-down"
-          color="grey darken-1 shrink"
-          size="32"
-      ></v-avatar>
     </v-app-bar>
 
     <v-main class="grey lighten-3">
       <v-container>
+
         <router-view/>
 
       </v-container>
--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home">
+  <div>
 
     <v-row>
       <v-col
@@ -7,22 +7,77 @@
           sm="2"
       >
         <v-sheet
-            rounded="lg"
             min-height="268"
+            rounded="lg"
         >
           <!--  -->
         </v-sheet>
       </v-col>
-
+      
       <v-col
           cols="12"
           sm="8"
       >
         <v-sheet
+            class="pl-3 pr-4"
             min-height="70vh"
             rounded="lg"
         >
-          <!--  -->
+
+          <div class="text-center pa-5">
+            <h2>Enter the ingredients for your recipe</h2>
+            <h3>Write from 1 to 3 ingredients</h3>
+          </div>
+
+          <v-row no-gutters>
+            <v-col
+                cols="12"
+                sm="8">
+
+              <v-text-field
+                  v-model="ingredient"
+                  clearable
+                  label="Ingredient"
+                  outlined
+                  :disabled="disableForm"
+                  @keyup.enter="addItem"
+              ></v-text-field>
+
+            </v-col>
+
+            <v-col
+                cols="12"
+                sm="4">
+              <v-btn
+                  :disabled="disableForm"
+                  block
+                  depressed
+                  class="ma-2"
+                  color="primary"
+                  dark
+                  @click="addItem"
+              >
+                Add Item
+              </v-btn>
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <v-col
+                cols="12"
+                sm="12"
+            >
+              <v-chip-group>
+                <v-chip v-for="(ingredient, index) in ingredients" :key="index"
+                     color="primary" @click="removeItem(index)"
+                >
+                  <span class="pr-2"> {{ingredient}} </span>
+                  <v-icon> mdi-close </v-icon>
+                </v-chip>
+              </v-chip-group>
+            </v-col>
+          </v-row>
+
         </v-sheet>
       </v-col>
 
@@ -31,8 +86,8 @@
           sm="2"
       >
         <v-sheet
-            rounded="lg"
             min-height="268"
+            rounded="lg"
         >
           <!--  -->
         </v-sheet>
@@ -47,7 +102,26 @@
 
 export default {
   name: 'Home',
-  components: {
-  }
+  data: () => ({
+    ingredient: null,
+    ingredients: [],
+    disableForm: false,
+  }),
+  methods: {
+    addItem() {
+      this.ingredients.push(this.ingredient);
+      this.ingredient = null;
+    },
+    removeItem(index){
+      if (this.ingredients.indexOf(index) == -1) {
+        this.ingredients.splice(index, 1);
+      }
+    }
+  },
+  watch: {
+    ingredients(val) {
+        this.disableForm = val.length >= 3 ? true : false;
+    }
+  },
 }
 </script>
