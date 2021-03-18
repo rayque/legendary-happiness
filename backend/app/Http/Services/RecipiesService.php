9a0d9d4bed4793e9a75a0a06e8b127fb370be8f1
rayque <rayque.oliveira@gmail.com>
2021-03-17 23:43:12 -0400

    add try catch

--- backend/app/Http/Services/RecipiesService.php
+++ backend/app/Http/Services/RecipiesService.php
@@ -5,44 +5,51 @@ namespace App\Http\Services;
 
 
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class RecipiesService
 {
     public function gerRecipies($params)
     {
-        if (!$params) {
-            return badRequest([
-                'message' => 'Insert at least one ingredient.'
-            ]);
-        }
-        $ingredients = explode(",", $params);
-        if (count($ingredients) > 3) {
-            return badRequest([
-                'message' => 'Insert a maximum of 3 ingredients.'
-            ]);
-        }
+        try {
+            if (!$params) {
+                return badRequest([
+                    'message' => 'Insert at least one ingredient.'
+                ]);
+            }
+            $ingredients = explode(",", $params);
+            if (count($ingredients) > 3) {
+                return badRequest([
+                    'message' => 'Insert a maximum of 3 ingredients.'
+                ]);
+            }
 
-        $response = Http::get('http://www.recipepuppy.com/api/?i=onions,garlic');
+            $response = Http::get('http://www.recipepuppy.com/api/?i=onions,garlic');
 
-        if ($response->failed()) {
-            serverError("Falha ao buscar buscar receiras");
-        }
+            if ($response->failed()) {
+                serverError("Falha ao buscar buscar receiras");
+            }
 
-        $recipies = array_map(function ($recipe) {
-            return [
-                'title' => $recipe['title'],
-                'ingredients' => $recipe['ingredients'],
-                'link' => $recipe['href'],
-                'gif' => $recipe['thumbnail'],
+            $recipies = array_map(function ($recipe) {
+                return [
+                    'title' => $recipe['title'],
+                    'ingredients' =>  explode(",", $recipe['ingredients']) ,
+                    'link' => $recipe['href'],
+                    'gif' => $recipe['thumbnail'],
+                ];
+            }, $response->collect()['results'] ?? []);
+
+
+            $data = [
+                'keywords' => $ingredients,
+                'recipies' => $recipies,
             ];
-        }, $response->collect()['results'] ?? []);
 
-
-        $data = [
-            'keywords' => $ingredients,
-            'recipies' => $recipies,
-        ];
-
-        return ok($data);
+            return ok($data);
+        } catch (\Exception $exception) {
+            Log::error($exception->getMessage());
+            Log::error($exception->getTraceAsString());
+            return serverError("Falha ao buscar buscar receiras");
+        }
     }
 }
